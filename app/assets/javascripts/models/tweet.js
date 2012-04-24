e16072ec040d0ddc4c0ed45e35e4230d3d5216bc
Sam Smoot <ssmoot@gmail.com>
2012-04-24 15:09:31 -0500

    Pushing progress so far.

---
+++ app/assets/javascripts/models/tweet.js
@@ -0,0 +1,3 @@
+var Tweet = Backbone.Model.extend({
+  
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneDemo::Application.config.secret_token = 'b08153339ddf2fa3bec19e6ea337faca7af97dc6b96dd641444901a2ad56b51583b13b168bcb8da7198ea913452b00e5b9fa1c7c562ef9d35cc142e32fe44698'
